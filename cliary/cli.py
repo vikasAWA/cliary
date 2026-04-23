605907932d0ded82bc6365c2ddc97864323de66a
vikasAWA <awasthivikas02@gmail.com>
2026-04-23 13:35:35 +0530

    clean project structure

--- cliary/cli.py
+++ cliary/cli.py
@@ -1,6 +1,13 @@
 import argparse
 import time
 from cliary.core import write_note, read_notes, search_notes
+from rich.console import Console
+from rich.panel import Panel
+
+
+console = Console()
+console.print(Panel("cliary ✍️\na quiet place for your thoughts", style="cyan"))
+
 
 
 def slow_print(text):
@@ -15,8 +22,14 @@ def main():
     parser.add_argument("command")
     parser.add_argument("arg", nargs="?")
     args = parser.parse_args()
-    
+
+        
     match args.command:
-        case "write": write_note()
-        case "read": read_notes(slow_print)
-        case "search": search_notes(args.arg) if args.arg else print('Please provide a keyword')
+        case "write": 
+            console.print("[bold green]Write your thought:[/bold green]")
+            write_note()
+        case "read": 
+            console.print("[bold yellow]Reading...[/bold yellow]\n")
+            read_notes(slow_print)
+        case "search": 
+            search_notes(args.arg) if args.arg else print('Please provide a keyword')
--- cliary/core.py
+++ cliary/core.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from datetime import datetime
 
 LOG_DIR = Path("logs")
-LOG_DIR.mkdir(exist_ok=True)
+LOG_DIR.mkdir(parents=True, exist_ok=True)
 
 
 def get_today_file():
@@ -14,17 +14,20 @@ def write_note():
     file = get_today_file()
     while True:
         note = input(">> ")
-        if note.strip().lower() == 'exit': break
+        if note.strip().lower() == 'exit': 
+            print("Saved. Exiting...")
+            break
         with open(file, "a") as f:
             f.write(note + "\n")
 
 
 def read_notes(slow_print):
-    try: file = get_today_file() 
-    except FileNotFoundError: ('No notes today')
+    file = get_today_file() 
+    if not file.exists(): 
+        slow_print('no notes today')
+        return
     with open(file) as f:
-        for line in f:
-            slow_print(line)
+        for line in f: slow_print(line)
 
 def search_notes(keyword):
     for file in LOG_DIR.glob("*.txt"):
